78e3fc42fe4561bec185512f1239364216505f7b
MadMaxxou <58481450+MadMaxxou@users.noreply.github.com>
2020-01-14 16:21:03 +0100

    maj

--- index.php
+++ index.php
@@ -10,10 +10,17 @@
 </head>
 <body id="body">
 
-<div id="includedContent"></div>
+    <div id="includedContent"></div>
 
-<br>
-<a>test</a>
+    <section>
+        <h1 style="color: white; text-align: center"> Un monde constructif breveté</h1>
+        </br>
+        <p style="color: white">Le principe constructif des modulaires MADERA repose sur une structure autoportante tridimensionnelle en bois assemblée à des panneaux de façades en ossature bois.
+
+            Avec ce système breveté, de multiples combinaisons deviennent possibles pour réaliser votre projet.
+
+            Ce procédé de montage simple est également réversible car il permet le démontage de tout ou partie de bâtiment et rend possible toutes modifications, agrandissements, transferts vers un autre lieu d’implantation…</p>
+    </section>
 <footer>
     <a>Contacts</a>
     <a>Mails</a>
