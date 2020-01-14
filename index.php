f8661263312cf4397a52349cce18636d251689ae
MadMaxxou <58481450+MadMaxxou@users.noreply.github.com>
2020-01-14 15:39:14 +0100

    d

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php incude "idex2.html";?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -9,11 +9,11 @@
     <link href=" https://cdn.discordapp.com/attachments/337935730045222923/666273882730266652/favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon" />
 </head>
 <body id="body">
-
-    <div id="includedContent"></div>
+<?php incude "idex2.html";?>
+<div id="includedContent"></div>
 
 <br>
-    <a>test</a>
+<a>test</a>
 <footer>
     <a>Contacts</a>
     <a>Mails</a>
