6b1628bc57e68ecfe6ec57287665e528ab2a4559
MadMaxxou <58481450+MadMaxxou@users.noreply.github.com>
2020-02-14 09:36:46 +0100

    a

---
+++ views/pages/404.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en" class="html1">
+
+<head>
+  <meta charset="utf-8">
+  <title>Page Not Found</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="../../css/404.css" media="all"/>
+  <link rel="stylesheet" type="text/css" href="css/menu.css" media="all"/>
+</head>
+
+<body class="body1">
+<?php include "../base.html"?>
+  <div id="content">
+    <h1 class="hn" style="text-align: center;%">Page Not Found</h1>
+    <p class="p1" style="text-align: center; margin-top: 1%">Sorry, but the page you were trying to view does not exist.</p>
+  </div>
+
+</body>
+
+</html>
+<!-- IE needs 512+ bytes: https://blogs.msdn.microsoft.com/ieinternals/2010/08/18/friendly-http-error-pages/ -->
